ed684b1f9b7c0ea4de527540719c1cf290960cf4
Daniel Maus <hakumennomodo@gmail.com>
2025-04-10 21:29:45 -0300

    SALVAR

--- WF.Login/WF.Login/Form1.Designer.cs
+++ WF.Login/WF.Login/Form1.Designer.cs
@@ -31,13 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
-            textBox1 = new TextBox();
             textBox2 = new TextBox();
             CADASTRAR = new Button();
             linkLabel1 = new LinkLabel();
-            label1 = new Label();
-            label2 = new Label();
             linkLabel2 = new LinkLabel();
+            label3 = new Label();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -49,28 +50,18 @@
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(419, 613);
+            panel1.Size = new Size(419, 492);
             panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(12, 105);
+            pictureBox1.Location = new Point(12, 60);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(394, 404);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = Color.Black;
-            textBox1.BorderStyle = BorderStyle.None;
-            textBox1.ForeColor = Color.Gray;
-            textBox1.Location = new Point(546, 88);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(316, 24);
-            textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
@@ -87,6 +78,7 @@
             CADASTRAR.BackColor = Color.DimGray;
             CADASTRAR.FlatAppearance.BorderSize = 0;
             CADASTRAR.FlatStyle = FlatStyle.Flat;
+            CADASTRAR.Font = new Font("Segoe UI", 15F);
             CADASTRAR.ForeColor = Color.Black;
             CADASTRAR.Location = new Point(457, 279);
             CADASTRAR.Name = "CADASTRAR";
@@ -98,60 +90,90 @@
             // linkLabel1
             // 
             linkLabel1.AutoSize = true;
-            linkLabel1.Location = new Point(631, 431);
+            linkLabel1.Location = new Point(631, 374);
             linkLabel1.Name = "linkLabel1";
             linkLabel1.Size = new Size(141, 25);
-            linkLabel1.TabIndex = 4;
+            linkLabel1.TabIndex = 0;
             linkLabel1.TabStop = true;
             linkLabel1.Text = "Recuperar senha";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.ForeColor = Color.Gray;
-            label1.Location = new Point(447, 87);
-            label1.Name = "label1";
-            label1.Size = new Size(67, 25);
-            label1.TabIndex = 5;
-            label1.Text = "LOGIN:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.ForeColor = Color.Gray;
-            label2.Location = new Point(455, 182);
-            label2.Name = "label2";
-            label2.Size = new Size(73, 25);
-            label2.TabIndex = 6;
-            label2.Text = "SENHA:";
             // 
             // linkLabel2
             // 
             linkLabel2.AutoSize = true;
-            linkLabel2.Location = new Point(656, 473);
+            linkLabel2.Location = new Point(656, 428);
             linkLabel2.Name = "linkLabel2";
             linkLabel2.Size = new Size(87, 25);
             linkLabel2.TabIndex = 7;
             linkLabel2.TabStop = true;
             linkLabel2.Text = "Cadastrar";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Silver;
+            label3.Location = new Point(454, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(474, 25);
+            label3.TabIndex = 8;
+            label3.Text = "__________________________________________________________________";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Silver;
+            label4.Location = new Point(454, 209);
+            label4.Name = "label4";
+            label4.Size = new Size(474, 25);
+            label4.TabIndex = 9;
+            label4.Text = "__________________________________________________________________";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Black;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Segoe UI", 15F);
+            textBox3.ForeColor = Color.DimGray;
+            textBox3.Location = new Point(457, 106);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(472, 40);
+            textBox3.TabIndex = 10;
+            textBox3.Text = "USUARIO:";
+            textBox3.Enter += textBox3_Enter;
+            textBox3.Leave += textBox3_Leave;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.Black;
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Font = new Font("Segoe UI", 15F);
+            textBox4.ForeColor = Color.DimGray;
+            textBox4.Location = new Point(454, 182);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(472, 40);
+            textBox4.TabIndex = 11;
+            textBox4.Text = "SENHA:";
+            textBox4.Enter += textBox4_Enter;
+            textBox4.Leave += textBox4_Leave;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(981, 613);
+            ClientSize = new Size(981, 492);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(linkLabel2);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(linkLabel1);
             Controls.Add(CADASTRAR);
             Controls.Add(textBox2);
-            Controls.Add(textBox1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Opacity = 0.85D;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -162,13 +184,14 @@
         #endregion
 
         private Panel panel1;
-        private TextBox textBox1;
         private TextBox textBox2;
         private Button CADASTRAR;
         private LinkLabel linkLabel1;
         private PictureBox pictureBox1;
-        private Label label1;
-        private Label label2;
         private LinkLabel linkLabel2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private TextBox textBox4;
     }
 }
--- WF.Login/WF.Login/Form1.cs
+++ WF.Login/WF.Login/Form1.cs
@@ -6,5 +6,30 @@ namespace WF.Login
         {
             InitializeComponent();
         }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "USUARIO:")
+                textBox3.Text = "";
+
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+                textBox3.Text = "USUARIO:";
+        }
+
+        private void textBox4_Enter(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "SENHA:")
+                textBox4.Text = "";
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+                textBox4.Text = "SENHA:";
+        }
     }
 }
